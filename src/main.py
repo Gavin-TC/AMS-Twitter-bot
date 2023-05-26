84313cd27d641f1248a40988e135cedb7bc52c10
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-26 18:19:12 -0400

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,42 @@
-import csvretriever
-import data
-import tweeter
+import time
+import threading
+import os
 
-tweet = tweeter()
-retriever = csvretriever()
+import csvretriever
+import data as dt
+
+allowed_to_track = True
+tracking = True
+
+time_to_sleep = 600  # seconds
+
+csv_directory = "src\\csvs"
 
 def main():
-    retriever.retrieve_csv()
-    oldest = data.return_oldest_csv()
-    latest = data.return_newest_csv()
+    input_thread = threading.Thread(target=check_input)
+    input_thread.start()
 
-def compare_dates():
-    retriever.retrieve_csv()
-    oldest = data.return_oldest_csv()
-    latest = data.return_newest_csv()
+    if len(os.listdir(csv_directory)) == 0:
+        print("Adding a csv!")
+        csvretriever.get_new_csv()
 
+    while allowed_to_track:
+        if tracking:
+            dt.compare_dates()
+            time.sleep(time_to_sleep)
+        else:
+            pass
+
+def check_input():
+    global allowed_to_track, tracking
+    time.sleep(1)
+    while allowed_to_track:
+        print()
+        user_input = input("Enter 'p' to pause tracking: ")
+
+        # Pause tracking.
+        if user_input == "p":
+            tracking = not tracking
 
 if __name__ == '__main__':
     main()
