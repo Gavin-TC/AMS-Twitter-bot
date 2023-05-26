7dfc4df605d2422bdb55fda3f313f0b33beb9d4f
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-26 18:18:59 -0400

    compares csv, if new incident, then tweet

--- src/data.py
+++ src/data.py
@@ -2,30 +2,71 @@ import tweeter
 import csv
 import os
 import pandas as pd
+from datetime import datetime
 
 import csvretriever as get_csv
-import csvs
 
 client = tweeter.client()
 csv_location = "https://www.gunviolencearchive.org/query/0484b316-f676-44bc-97ed-ecefeabae077/export-csv"
 
-directory = get_csv.folder_path
+def compare_dates():
+    # get_csv.retrieve_csv()
+    get_csv.get_new_csv()
 
-def data_clear():
-    for filename in os.listdir(directory):
-        filepath = os.path.join(directory, filename)
-        os.remove(filepath)
+    oldest_csv = os.path.join(get_csv.directory, get_csv.return_oldest_csv())
+    latest_csv = os.path.join(get_csv.directory, get_csv.return_oldest_csv())
+
+    now = datetime.now()
+    today = now.strftime("%B %d, %Y")
+
+    old_incidents = 0
+    new_incidents = 0
     
-data_clear()
-
-def return_oldest_csv():
-    for filename in os.listdir(directory):
-        file = filename
-        break
-    return file
-
-def return_newest_csv():    
-    for filename in os.listdir(directory):
-        file = filename
+    # Check oldest CSV
+    with open(oldest_csv, 'r') as file:
+        print("Oldest csv is " + str(file.name))
+        csvreader = csv.DictReader(file)
+        for row in csvreader:
+            if row["Incident Date"] == today:
+                old_incidents += 1
     
-    return file
+    # Check newest CSV
+    with open(latest_csv, 'r') as file:
+        print("Newest csv is " + str(file.name))
+        csvreader = csv.DictReader(file)
+        for row in csvreader:
+            if row["Incident Date"] == today:
+                new_incidents += 1
+    
+    if old_incidents > 0:
+        if new_incidents > old_incidents:
+            with open(latest_csv, 'r') as file:
+                print("Newest csv is " + str(file.name))
+                csvreader = csv.DictReader(file)
+                for row in csvreader:
+                    if row["Incident Date"] == today:
+                        state = row["State"]
+                        injured = row["# Victims Injured"]
+                        killed = row["# Victims Injured"]
+                    num_mass_shootings = sum(1 for row in csvreader)
+            print("There has been a new shooting today.")
+            
+            tweeter.tweet(client, f"""
+A mass shooting has occured in {state}.
+
+{injured} people were injured.
+{killed} people were killed.
+
+This makes {num_mass_shootings} mass shootings so far this year.
+            """)
+            
+            print("Incident tweeted.")
+        else:
+            print("There has not been a new shooting today.")
+    else:
+        print("There has not been any shootings so far today.")
+    
+    # Remove the oldest CSV effectively making the newest CSV the new oldest.
+    #get_csv.remove_csv(oldest_csv)
+    os.remove(oldest_csv)
+
