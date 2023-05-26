88ed5f218f0a8c99308a709b20f68f79c2c1d10b
Gavin C <59036563+Gavin-TC@users.noreply.github.com>
2023-05-26 03:46:34 -0400

    upload

---
+++ src/data.py
@@ -0,0 +1,8 @@
+import main
+import csv
+
+client = main.client()
+
+csv_location = "https://www.gunviolencearchive.org/query/0484b316-f676-44bc-97ed-ecefeabae077/export-csv"
+
+# check for change in a mass shootings database, then tweet.
