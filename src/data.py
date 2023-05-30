13b2a3001d829ba3a743d8f680dd5a23800d4b71
Gavin-TC <gtclonts@gmail.com>
2023-05-30 16:38:55 -0400

    fixing some oversights

--- src/data.py
+++ src/data.py
@@ -15,7 +15,7 @@ csv_location = "https://www.gunviolencearchive.org/query/0484b316-f676-44bc-97ed
 # TODO: basically, just tweet the # of injured/killed, # of incidents, maybe more?
 
 def make_comparison():
-    #ret.get_new_csv()
+    ret.get_new_csv()
     directory = ret.working_directory
     
     # put the csvs into variables
@@ -134,93 +134,6 @@ This makes {shootings_on_date} shooting(s) on {date}.
                 time.sleep(5)
     
     # remove the old file
-    #os.remove(oldest_csv)
+    os.remove(oldest_csv)
 
 make_comparison()
-
-# def compare_dates():
-#     get_csv.get_new_csv()
-
-#     oldest_csv = os.path.join(get_csv.working_directory, get_csv.return_oldest_csv())
-#     latest_csv = os.path.join(get_csv.working_directory, get_csv.return_newest_csv())
-
-#     now = datetime.now()
-#     today = now.strftime("%B %d, %Y")
-
-#     old_incidents = 0
-#     new_incidents = 0
-    
-#     # the incidents that have been detected and tweeted
-#     reported_incidents = []
-    
-#     # the incidents inside the downloaded csv
-#     total_incidents = []
-    
-#     # todo: i would like to check the last 10 entries in the csv and compare those.
-#     # todo: have to tweet out the date now. 
-    
-#     # Check oldest CSV
-#     with open(oldest_csv, 'r') as file:
-#         print("Oldest csv is " + str(file.name))
-#         csvreader = csv.DictReader(file)
-#         for row in csvreader:
-#             old_incidents += 1
-    
-#     # Check newest CSV
-#     with open(latest_csv, 'r') as file:
-#         print("Newest csv is " + str(file.name))
-#         csvreader = csv.DictReader(file)
-#         for row in csvreader:
-#            new_incidents += 1
-
-#     print(f"{old_incidents} old incidents")
-#     print(f"{new_incidents} new incidents")
-
-#     difference = new_incidents - old_incidents
-    
-#     print(difference > 0)
-
-#     # if we have any incidents today, begin checks.
-#     # if old_incidents >= 0:
-#     # if there is a positive difference in incidents, continue
-#     if difference > 0:
-#         # now we need to read the oldest of the latest incident.
-#         # i.e. 1 at 3pm, 1 at 4pm. read the one at 3pm first, then 4pm.
-#         with open(latest_csv, 'r') as file:
-#             csvreader = csv.DictReader(file)
-#             mass_shootings = []
-
-#             for row in csvreader:
-#                 for x in range(difference):
-#                     date = row["Incident Date"]
-#                     city = row["City Or County"]
-#                     state = row["State"]
-#                     injured = row["# Victims Injured"]
-#                     killed = row["# Victims Killed"]
-#                     mass_shootings.append({"date": date, "city": city, "state": state, "injured": injured, "killed": killed})
-            
-#             num_to_print = min(difference, len(mass_shootings))
-            
-#             for i in range(num_to_print):
-#                 shooting = mass_shootings[i]
-#                 date = shooting["date"]
-#                 city = shooting["city"]
-#                 state = shooting["state"]
-#                 injured = shooting["injured"]
-#                 killed = shooting["killed"]
-#                 total_shootings_today = len(mass_shootings)
-#                 #tweeter.tweet(client, f"""
-#                 print(f"""
-#                 A mass shooting has occurred on {date} in {city}, {state}.
-
-#                 {injured} people were injured.
-#                 {killed} people were killed.
-#                         """)
-#     else:
-#         print("There has not been any shootings so far today.")
-    
-#     # Remove the oldest CSV effectively making the newest CSV the new oldest.
-#     time.sleep(0.5)
-#     os.remove(oldest_csv)
-#     #get_csv.remove_csv(oldest_csv)
-
